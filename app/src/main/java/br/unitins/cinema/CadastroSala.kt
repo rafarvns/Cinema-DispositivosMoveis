ae630c7d1a780709b050132213851fd98cf16ce2
PazJulia <paz.julia7@gmail.com>
2019-04-12 10:12:10 -0300

    cadastro de sala e sessão

--- app/src/main/java/br/unitins/cinema/CadastroSala.kt
+++ app/src/main/java/br/unitins/cinema/CadastroSala.kt
@@ -1,12 +1,34 @@
 package br.unitins.cinema
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class CadastroSala : AppCompatActivity() {
+class CadastroSala : Activity() {
+
+    private var edtnSala: EditText? = null
+    private var edtqPoltrona: EditText? = null
+    private var bcadSala: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_sala)
+
+        edtnSala = findViewById(R.id.edtnSala)
+        edtqPoltrona = findViewById(R.id.edtqPoltrona)
+        bcadSala = findViewById(R.id.bcadSala)
+
+        if (edtnSala!!.text.toString() != "") {
+            if (edtqPoltrona!!.text.toString() != "") {
+
+                bcadSala!!.setOnClickListener { }
+
+            } else
+                Toast.makeText(this, "Preencha o campo da Poltrona", Toast.LENGTH_SHORT)
+        } else
+            Toast.makeText(this, "Preencha o campo da sala", Toast.LENGTH_SHORT)
+
     }
 }
---
+++ app/src/main/java/br/unitins/cinema/CadastroSessao.kt
@@ -0,0 +1,12 @@
+package br.unitins.cinema
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroSessao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_sessao)
+    }
+}
