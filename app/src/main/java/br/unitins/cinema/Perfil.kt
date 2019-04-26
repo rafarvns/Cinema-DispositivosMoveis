234e16e99f9ce457e52e9080d42ea70fc7341aa2
PazJulia <paz.julia7@gmail.com>
2019-04-26 10:18:24 -0300

    1

--- app/src/main/java/br/unitins/cinema/Perfil.kt
+++ app/src/main/java/br/unitins/cinema/Perfil.kt
@@ -3,7 +3,7 @@ package br.unitins.cinema
 import android.content.Intent
 import android.os.Bundle
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_perfil.*
 
 class Perfil : AppCompatActivity() {
