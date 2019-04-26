87ff1db20f3500004fc162be11c33692d7c2deed
PazJulia <paz.julia7@gmail.com>
2019-04-26 01:04:45 -0300

    1

--- app/src/main/java/br/unitins/cinema/CadastroSala.kt
+++ app/src/main/java/br/unitins/cinema/CadastroSala.kt
@@ -2,7 +2,6 @@ package br.unitins.cinema
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -18,7 +17,7 @@ class CadastroSala : Activity() {
 
         edtnSala = findViewById(R.id.edtnSala)
         edtqPoltrona = findViewById(R.id.edtqPoltrona)
-        bcadSala = findViewById(R.id.bcadSala)
+        bcadSala = findViewById(R.id.btcadSala)
 
         if (edtnSala!!.text.toString() != "") {
             if (edtqPoltrona!!.text.toString() != "") {
